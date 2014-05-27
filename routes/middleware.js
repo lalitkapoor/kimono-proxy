126d36f647c2bb703b5f225253cb4e43f97ff73a
Lalit Kapoor <lalitkapoor@gmail.com>
2014-05-27 05:02:09 -0500

    fix formatting

--- routes/middleware.js
+++ routes/middleware.js
@@ -12,47 +12,50 @@ router.all('*', authCheck)
 // list of all middleware
 router.get('/'
 , function (req, res) {
-  var sql = 'SELECT * FROM middleware'
+    var sql = 'SELECT * FROM middleware'
 
-  db.query(sql, function (error, rows, result) {
-    if (error) return console.error(error.stack), res.json(500)
-    return res.json(200, rows)
-  })
-})
+    db.query(sql, function (error, rows, result) {
+      if (error) return console.error(error.stack), res.json(500)
+      return res.json(200, rows)
+    })
+  }
+)
 
 
 // get single middleware
 router.get('/:id'
 , function (req, res) {
-  var sql = 'SELECT * FROM middleware WHERE id = $1'
-  var values = [req.params.id]
+    var sql = 'SELECT * FROM middleware WHERE id = $1'
+    var values = [req.params.id]
 
-  db.query.first(sql, values, function (error, row) {
-    if (error) return console.error(error.stack), res.json(500)
-    return res.json(200, row)
-  })
-})
+    db.query.first(sql, values, function (error, row) {
+      if (error) return console.error(error.stack), res.json(500)
+      return res.json(200, row)
+    })
+  }
+)
 
 // create middleware
 // Schema + contents: {"filename": "content"}
 router.post('/'
 , function (req, res) {
-  var sql = 'INSERT INTO middleware ("userId", "name", "description", "type", "repo") '
-          + 'VALUES ($1, $2, $3, $4, $5) RETURNING *'
-  var values = [
-    req.user.id
-  , req.body.name
-  , req.body.description
-  , req.body.type
-  , req.body.repo
-  ]
+    var sql = 'INSERT INTO middleware ("userId", "name", "description", "type", "repo") '
+            + 'VALUES ($1, $2, $3, $4, $5) RETURNING *'
+    var values = [
+      req.user.id
+    , req.body.name
+    , req.body.description
+    , req.body.type
+    , req.body.repo
+    ]
 
-  db.query.first(sql, values, function (error, row) {
-    console.log(arguments)
-    if (error) return console.error(error.stack), res.json(500)
-    return res.json(201, row)
-  })
-})
+    db.query.first(sql, values, function (error, row) {
+      console.log(arguments)
+      if (error) return console.error(error.stack), res.json(500)
+      return res.json(201, row)
+    })
+  }
+)
 
 
 // modify middleware that you are authorized to modify
@@ -61,13 +64,17 @@ router.post('/'
 router.patch('/:id'
 , middlewareOwnerCheck
 , function (req, res) {
-  return res.send(200)
-})
+    return res.send(200)
+  }
+)
 
 
 // delete middleware that you are authorized to modify
-router.delete('/:id', middlewareOwnerCheck, function (req, res) {
-  return res.send(200)
-})
+router.delete('/:id'
+, middlewareOwnerCheck
+, function (req, res) {
+    return res.send(200)
+  }
+)
 
 module.exports = router
