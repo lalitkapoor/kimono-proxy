b7eab0ddf81e259f38600d3b08103fd9631c9b5c
Lalit Kapoor <lalitkapoor@gmail.com>
2014-05-27 04:49:43 -0500

    check for existence of logged in user instead of session object

--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -1,6 +1,5 @@
 module.exports = function (req, res, next) {
   if (/^\/v1\/auth\/.*/i.test(req.path)) return next()
-  if (!req.session || !req.session.github || !req.session.github.token)
-    return res.redirect('/v1/auth/github')
+  if(!req.user) return res.redirect('/v1/auth/github')
   return next()
 }
