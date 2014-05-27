13024d4baa7c208916c67e3d5cecb2dc865eabd6
Lalit Kapoor <lalitkapoor@gmail.com>
2014-05-27 04:57:37 -0500

    list and create middleware

---
+++ middleware/middleware-owner-check.js
@@ -0,0 +1,12 @@
+var db = require('../db')
+
+module.exports = function (req, res, next) {
+  var sql = 'SELECT id FROM middleware WHERE id = $1 AND userId = $2'
+  var values = [req.params.id, req.user.id]
+
+  db.query.first(sql, values, function (error, row) {
+    if (error) return console.error(error.stack), res.json(500)
+    if (!row) return res.json(401)
+    return next()
+  })
+}
--- routes/middleware.js
+++ routes/middleware.js
@@ -3,42 +3,58 @@ var _ = require('lodash')
 var Promise = require('bluebird')
 var router = require('express').Router()
 var authCheck = require('../middleware/auth-check')
+var middlewareOwnerCheck = require('../middleware/middleware-owner-check')
+var db = require('../db')
 
 router.all('*', authCheck)
 
-router.get('/', function (req, res) {
-  // list of all middleware (including other users)
+
+// list of all middleware (including other users)
+router.get('/'
+, function (req, res) {
+  var sql = 'SELECT * FROM middleware'
+
+  db.query(sql, function (error, rows, result) {
+    if (error) return console.error(error.stack), res.json(500)
+    return res.json(200, rows)
+  })
+})
+
+
+// create middleware
+// Schema + contents: {"filename": "content"}
+router.post('/'
+, function (req, res) {
+  var sql = 'INSERT INTO middleware ("userId", "name", "description", "type", "repo") '
+          + 'VALUES ($1, $2, $3, $4, $5) RETURNING *'
+  var values = [
+    req.user.id
+  , req.body.name
+  , req.body.description
+  , req.body.type
+  , req.body.repo
+  ]
+
+  db.query.first(sql, values, function (error, row) {
+    console.log(arguments)
+    if (error) return console.error(error.stack), res.json(500)
+    return res.json(201, row)
+  })
+})
+
+
+// modify middleware that you are authorized to modify
+// create middleware-owner-check middleware
+// create repo in user's github & add files
+router.patch('/:id'
+, middlewareOwnerCheck
+, function (req, res) {
   return res.send(200)
 })
 
-  // EXPOSED SCHEMA
-  // {
-  //   id: int
-  // , name: string
-  // , type: enum('transform', 'filter')
-  // , repo: url // github repo
-  // , documentation: string // ideally markdown
-  // , userId: int // use github id
-  // , username: string // use github username
-  // , createdAt: timestamp w/timezone
-  // }
 
-router.post('/', function (req, res) {
-  // create middleware
-  // Schema + contents: {
-  // "filename": "content"}
-  return res.send(201)
-})
-
-router.patch('/:id', function (req, res) {
-  // modify middleware that you are authorized to modify
-  // create middleware-owner-check middleware
-  // create repo in user's github & add files
-  return res.send(200)
-})
-
-router.delete('/:id', function (req, res) {
-  // delete middleware that you are authorized to modify
+// delete middleware that you are authorized to modify
+router.delete('/:id', middlewareOwnerCheck, function (req, res) {
   return res.send(200)
 })
 
