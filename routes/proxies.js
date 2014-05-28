ecfe1b2671e9561401389ab19c2b4bfcf8b1c994
Lalit Kapoor <lalitkapoor@gmail.com>
2014-05-27 19:19:08 -0500

    list, get, create, delete proxies

--- routes/proxies.js
+++ routes/proxies.js
@@ -3,79 +3,114 @@ var _ = require('lodash')
 var Promise = require('bluebird')
 var router = require('express').Router()
 var authCheck = require('../middleware/auth-check')
+var proxyOwnerCheck = require('../middleware/proxy-owner-check')
+var db = require('../db')
 
 router.all('*', authCheck)
 
-router.get('/', function (req, res) {
-  // list of all proxies (including other users)
-  return res.send(200)
-})
 
-  // EXPOSED SCHEMA
-  // {
-  //   id: int
-  // , name: string
-  // , subdomain: string
-  // , api: url // api proxying to
-  // , documentation: string // ideally markdown
-  // , userId: int // use github id
-  // , username: string // use github username
-  // , createdAt: timestamp w/timezone
-  // , enabled: boolean
-  // }
+// list of all proxies
+router.get('/'
+, function (req, res) {
+    var sql = 'SELECT * FROM proxies'
 
-router.post('/', function (req, res) {
-  // create proxy
+    db.query(sql, function (error, rows, result) {
+      if (error) return console.error(error.stack), res.send(500)
+      return res.json(200, rows)
+    })
+  }
+)
 
-  // {
-  //   name: ''
-  // , url: ''
-  // , documentation: 'MARKDOWN text here'
-  // }
-  return res.send(201)
-})
+// get single proxies
+router.get('/:id'
+, function (req, res) {
+    var sql = 'SELECT * FROM proxies WHERE id = $1'
+    var values = [req.params.id]
 
-router.patch('/:id', function (req, res) {
-  // modify proxy that you are authorized to modify
-  // create proxy-owner-check middleware
-  return res.send(200)
-})
+    db.query.first(sql, values, function (error, row) {
+      if (error) return console.error(error.stack), res.send(500)
+      return res.json(200, row)
+    })
+  }
+)
 
-router.delete('/:id', function (req, res) {
-  // delete proxy that you are authorized to modify
-  return res.send(200)
-})
 
-router.get('/:id/middleware', function (req, res) {
-  // list all middleware for the given proxy
-  return res.send(200)
-})
+// create proxy
+router.post('/'
+, function (req, res) {
+    var sql = 'INSERT INTO proxies '
+      + '("userId", "name", "description", "documentation", "subdomain", "url", "middleware") '
+      + 'VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *'
+    var values = [
+      req.user.id
+    , req.body.name
+    , req.body.description || null
+    , req.body.documentation || null
+    , req.body.subdomain
+    , req.body.url
+    , JSON.stringify(req.body.middleware || [])
+    ]
 
-router.put('/:id/middleware', function (req, res) {
-  // modify proxy's middleware
-  return res.send(200)
-})
+    db.query.first(sql, values, function (error, row) {
+      if (error) return console.error(error.stack), res.send(500)
+      return res.json(201, row)
+    })
+  }
+)
 
-router.post('/:id/start', function (req, res) {
-  // start your proxy
-  // check if docker container is running
-  // if not running, start it, else ignore
-  // if already running ignore
-  return res.send(200)
-})
 
-router.post('/:id/stop', function (req, res) {
-  // stop your proxy
-  // check if docker container is running
-  // if so stop it, else ignore
-  return res.send(200)
-})
+// modify proxy that you are authorized to modify
+router.patch('/:id'
+, proxyOwnerCheck
+, function (req, res) {
+    return res.send(200)
+  }
+)
 
-router.post('/:id/restart', function (req, res) {
-  // restart your proxy
-  // check if docker container is running
-  // if so stop it and start it else start it
-  return res.send(200)
-})
+
+// delete proxy that you are authorized to modify
+router.delete('/:id'
+, proxyOwnerCheck
+, function (req, res) {
+    var sql = 'DELETE FROM proxies WHERE id = $1'
+    var values = [req.params.id]
+
+    db.query(sql, values, function (error, row, result) {
+      if (error) return console.error(error.stack), res.send(500)
+      return res.send(200)
+    })
+  }
+)
+
+
+// start your proxy
+router.post('/:id/start'
+, function (req, res) {
+    // check if docker container is running
+    // if not running, start it, else ignore
+    // if already running ignore
+    return res.send(200)
+  }
+)
+
+
+// stop your proxy
+router.post('/:id/stop'
+, function (req, res) {
+    // check if docker container is running
+    // if so stop it, else ignore
+    return res.send(200)
+  }
+)
+
+
+// restart your proxy
+router.post('/:id/restart'
+, function (req, res) {
+    // check if docker container is running
+    // if so stop it and start it else start it
+    return res.send(200)
+  }
+)
 
 module.exports = router
