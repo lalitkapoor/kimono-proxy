e28f0b8c79a998ec9b22521192652c9dd672c57d
Lalit Kapoor <lalitkapoor@gmail.com>
2014-05-30 06:53:46 -0500

    cleanup: don't need proxies.jsx any more

--- public/js/page-proxies.jsx
+++ public/js/page-proxies.jsx
@@ -2,7 +2,17 @@
 
 var PageProxies = React.createClass({
   getInitialState: function () {
-    return {}
+    return {data: []}
+  }
+
+, componentWillMount: function () {
+    var self = this
+    superagent
+    .get('/v1/proxies')
+    .end(function (error, res) {
+      if (error) return console.error(error)
+      self.setState({data: res.body})
+    })
   }
 
 , componentDidMount: function () {
@@ -21,6 +31,10 @@ var PageProxies = React.createClass({
   }
 
 , render: function () {
+    var proxyNodes = this.state.data.map(function (proxy, index) {
+      return Proxy({data: proxy}, proxy.name)
+    })
+
     return (
       <div className="row">
         <div className="col-xs-5 col-md-5">
@@ -31,7 +45,9 @@ var PageProxies = React.createClass({
               new
             </button>
           </h2>
-          <Proxies />
+          <div className="list-group">
+            {proxyNodes}
+          </div>
         </div>
         <div className="col-xs-7 col-md-7">
           <h2>Details</h2>
