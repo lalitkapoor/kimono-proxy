f87780d26d5098c108878142b1e956daf3c4da89
Lalit Kapoor <lalitkapoor@gmail.com>
2014-05-30 07:11:32 -0500

    show proxy running status

--- public/js/proxy.jsx
+++ public/js/proxy.jsx
@@ -16,8 +16,14 @@ var Proxy = React.createClass({
   }
 
 , render: function () {
+    var statusColor = "";
+    if (this.props.data.status === 'stopped') statusColor = 'alert-danger'
+    if (this.props.data.status === 'starting') statusColor = 'alert-warning'
+    if (this.props.data.status === 'running') statusColor = 'alert-success'
+
     return (
       <a className="list-group-item proxy" onClick={this.handleClick}>
+        <span className={"type badge " + statusColor}>{this.props.data.status}</span>
         <h4 className="name">{this.props.data.name}</h4>
         <p className="description">{this.props.data.description}</p>
       </a>
